7b41dd1739488ed8db34d59ee4f38de2606615da
tsava <sava.taras@gmail.com>
2021-01-01 21:15:46 +0100

    Added weapon component

--- Shooter/Source/Shooter/SCharacter.cpp
+++ Shooter/Source/Shooter/SCharacter.cpp
@@ -49,6 +49,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComponen
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
 void ASCharacter::MoveForward(float Value)
